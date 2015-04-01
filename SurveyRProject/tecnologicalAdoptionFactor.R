bb7d0834bb6aa193224ac7ea1de119e464acca70
sfohart <sfohart@gmail.com>
2015-04-01 18:25:33 -0300

    alterações no grafico de fatores tecnologicos

--- SurveyRProject/tecnologicalAdoptionFactor.R
+++ SurveyRProject/tecnologicalAdoptionFactor.R
@@ -1,4 +1,10 @@
+#install.packages(c('reshape2','ggplot2','scales', 'grid'))
 load("surveyData.RData")
+
+require(reshape2)
+require(ggplot2)
+require(scales)
+require(grid)
 
 fator.tecnologico.labels <- c(
   "Facilidade de testar e de ser avaliado por usuários de um modo geral",
@@ -11,7 +17,7 @@ fator.tecnologico.labels <- c(
 )
 
 # Quebrando o texto automaticamente (Só tirei os \n do vetor de cima e acrescentei as 3 linhas abaixo)
-tamanho <- 40 # O limite de caracteres (ele vai quebrar no espaço anterior a palavra que ultrapassa o limite)
+tamanho <- 35 # O limite de caracteres (ele vai quebrar no espaço anterior a palavra que ultrapassa o limite)
 (fator.tecnologico.labels <- strwrap(fator.tecnologico.labels, tamanho, simplify=F)) # Cria uma lista onde cada elemento é um vetor com até 40 caracteres. 
 (fator.tecnologico.labels <- mapply(paste, fator.tecnologico.labels, collapse='\n')) # Monta o vetor com as labels.
 
@@ -26,52 +32,76 @@ fator.tecnologico.dados <- cbind(
   table(as.numeric(data[,23]))
 )
 
-# Analisando respostas 'muita influência' e 'influência considerável'
-# fator.tecnologico.resultado <- rbind(
-#   fator.tecnologico.dados[3:4,]
-# )
-
-
-# fator.tecnologico.resultado <- data.frame(
-#   fatores = fator.tecnologico.labels,
-#   total = colSums(fator.tecnologico.dados[3:4,])
-# )
-# 
-# 
-# fator.tecnologico.resultado <- rbind(
-#   mapply(fator.tecnologico.resultado, 7, FUN = function(r,c) round(r / 152 * 100, digits=2))
-# )  
-
-fator.tecnologico.resultado = data.frame(
-  fatores = fator.tecnologico.labels,
-  total = colSums(fator.tecnologico.dados[3:4,])  
+fator.tecnologico.resultado <- as.data.frame(
+  rbind(
+    colSums(fator.tecnologico.dados[3:4,]),
+    colSums(fator.tecnologico.dados[1:2,])
+    )
   )
 
-fator.tecnologico.percentagens = data.frame(
-  fatores = fator.tecnologico.labels,
-  total = mapply(
-    colSums(fator.tecnologico.dados[3:4,]),
-    FUN = function(r,c) round(r / 152 * 100, digits=2)
+fator.tecnologico.percentagens <- as.data.frame(
+  rbind(    
+    mapply(
+      colSums(fator.tecnologico.dados[3:4,]),
+      FUN = function(r,c) round(r / 152 * 100, digits=2)
+      ),
+    mapply(
+      colSums(fator.tecnologico.dados[1:2,]),
+      FUN = function(r,c) round(r / 152 * 100, digits=2)
+      )
     )
+  )
+
+fator.tecnologico.resultado <- cbind(
+  fator.tecnologico.resultado,
+  Label = c("influência considerável ou alta","pouca ou nenhuma influência")
+  #Label = c("pouca ou nenhuma influência","influência considerável ou alta")
+  )
+
+fator.tecnologico.percentagens <- cbind(
+  fator.tecnologico.percentagens,
+  Label = c("influência considerável ou alta","pouca ou nenhuma influência")
+  #Label = c("pouca ou nenhuma influência","influência considerável ou alta")
 )
 
+# Para usar o ggplot2 os dados precisam estar nesse formato.
+fator.tecnologico.resultado.agrupado <- reshape(
+  fator.tecnologico.resultado, 
+  varying = list(c('V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7')), 
+  direction='long', 
+  idvar='Label', 
+  times=fator.tecnologico.labels
+  ) 
 
-require(ggplot2)
-require(scales)
+fator.tecnologico.percentagens.agrupado <- reshape(
+  fator.tecnologico.percentagens, 
+  varying = list(c('V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7')), 
+  direction='long', 
+  idvar='Label', 
+  times=fator.tecnologico.labels
+  ) 
 
 
-fator.tecnologico.grafico <-
-  ggplot(data = fator.tecnologico.percentagens, aes(x = fatores, y = total, color = "black"), size = 3.5) +
-  theme(text = element_text(size=18, face = "bold")) +
-  geom_bar(stat = "identity", position = "dodge", fill="#99CCFF", colour="black") +
-  geom_text(aes(label = paste(total, "%", " ")), hjust = 1.2, color="black", fontface = "bold") +
-  #scale_y_continuous(labels = percent_format()) +  
-  coord_flip() +
-  xlab('Fatores de adoção') +
-  ylab('Muita Influência + Influência Considerável (%)') +
-  ggtitle("Na sua opinião, em que grau estes fatores influenciam\n a adoção de software livre do ponto de vista tecnológico?\n")
+fator.tecnologico.resultado.agrupado.grafico <- ggplot(aes(x=time, y = V1, fill = Label, position = "fill"), data=fator.tecnologico.resultado.agrupado) + # Informa os dados
+  #theme_classic() +
+  theme(legend.position="top",  text = element_text(size=15, face = "bold")) +
+  geom_bar(stat='identity',  colour = "black", ymax=152) + # Informa que tu quer um gráfico de barras
+  
+  geom_text (aes(x=time, y=V1, fill=Label, label = V1), ymax=152, fontface = "bold", color="black", hjust=-0.25, size = 4.5) +
+  scale_y_continuous(limit =  c(0,152)) +
+  # Inverte os eixos X e Y
+  coord_flip() + 
+  # Coloca titulo no eixo X
+  ylab('Frequência (%)') + 
+  # Coloca titulo do eixo Y
+  xlab('Fatores de adoção') +  
+  # Coloca titulo no grafico
+  ggtitle("Na sua opinião, em que grau estes fatores influenciam\n a adoção de software livre do ponto de vista tecnológico?") + 
+  # Coloca titulo na legenda
+  scale_fill_manual('Legenda', values = c("#66CC99","#CC6666"))
 
-fator.tecnologico.grafico
+
+fator.tecnologico.resultado.agrupado.grafico
 
 
-ggsave(fator.tecnologico.grafico, file="pictures/06-fator-tecnologico.png")
+ggsave(fator.tecnologico.resultado.agrupado.grafico, file="pictures/06-fator-tecnologico-percentagens.png")
